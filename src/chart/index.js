c65162af2b24b8256b8f5b26cb14c291c436eb53
Klaus <839887585@qq.com>
2019-10-10 23:47:07 +0800

    初始化

---
+++ src/chart/index.js
@@ -0,0 +1,37 @@
+import React, {useState, useRef,useEffect} from 'react';
+import echarts from 'echarts';
+
+export default function Chart (){
+    const container = useRef(null);
+
+    useEffect(()=>{
+        var myChart = echarts.init(container.current);
+        var option = {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+        myChart.setOption(option);
+
+        return ()=>{
+            myChart.dispose();
+        };
+    });
+
+    return (
+        <div ref={container} style={{height:500}}></div>
+    );
+}
